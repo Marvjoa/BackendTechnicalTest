a82674b2a5641ea0529a367333e030a82781cff9
Marvin Ojeda <marvinjoseojeda2@gmail.com>
2025-07-30 03:35:50 -0600

    feat: creacion de los modelos para cliente, cuenta y transaccion

---
+++ BackendTechnicalTest.Models/Accounts/Account.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BackendTechnicalTest.Models.Clients;
+using BackendTechnicalTest.Models.Transactions;
+
+namespace BackendTechnicalTest.Models.Accounts;
+
+public class Account: Keyed
+{
+    [Required]
+    public string AccountNumber { get; set; } = null!;
+    
+    [Required]
+    public decimal Balance { get; set; }
+
+    #region Foreign Keys
+
+    [Required]
+    [ForeignKey(nameof(ClientId))]
+    public Guid ClientId { get; set; }
+    
+    public Client Client { get; set; } = null!;
+
+    public ICollection< Transaction> Transactions { get; set; } = new List<Transaction>();
+    #endregion
+
+}
---
+++ BackendTechnicalTest.Models/Transactions/Transaction.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BackendTechnicalTest.Models.Accounts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendTechnicalTest.Models.Transactions;
+
+public class Transaction : Keyed
+{
+    [Required]
+    public DateTime Date { get; set; } = DateTime.UtcNow;
+    
+    [Required]
+    public decimal Amount { get; set; }
+    
+    [Required]
+    public TransactionType Type { get; set; }
+    
+    [Required]
+    [Precision(16, 4)]
+    public decimal Balance { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    #region Foreign Keys
+
+    [Required]
+    [ForeignKey(nameof(AccountId))]
+    public Guid AccountId { get; set; }
+
+    public Account Account { get; set; } = null!;   
+    
+    #endregion
+}
